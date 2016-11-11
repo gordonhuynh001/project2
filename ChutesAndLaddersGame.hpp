e28d87be16ca6030d026595ef4a7812bfd1cafc7
gordonhuynh001 <gordonhuynh001@csu.fullerton.edu>
2016-11-10 21:30:16 -0800

    Update ChutesAndLaddersGame.hpp

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -1,7 +1,3 @@
-//
-//  ChutesAndLaddersGame.hpp
-//
-
 #pragma once
 
 #include <stdio.h>
@@ -18,34 +14,40 @@ const int MIN_NUMBER_OF_PLAYERS = 2;
 
 class ChutesAndLaddersGame {
 public:
-   // TODO: implement the constructor with all your team members
-   // constructor with the default value of 2 minimim players
-   ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
+	// TODO: implement the constructor with all your team members
+	// constructor with the default value of 2 minimum players
+	ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
 
-   // TODO: implement the destructor
-   // destructor
-   ~ChutesAndLaddersGame();
-   
-   // accessors
-   // TO DO: implement the accessor to get the number Of Players
-   long getNumberOfPlayers() {
-      // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
-   }
-   string getWinner() { return winner; }
-   
-   // TODO: implement this function properly
-   // reset the game - (1) this will reset the position of each player to zero
-   void resetGame();
-   
-   // TODO: implement this function properly
-   // play the Chutes and Ladders game
-   void playGame();
+	// TODO: implement the destructor
+	// destructor
+	~ChutesAndLaddersGame();
+
+	// accessors
+	// TO DO: implement the accessor to get the number Of Players
+	long getNumberOfPlayers() {
+		// TODO: implement this function properly
+		return nPlayers;
+	}
+
+	string getWinner() {
+		return winner;
+	}
+
+	// TODO: implement this function properly
+	// reset the game - (1) this will reset the position of each player to zero
+	void resetGame();
+
+	// TODO: implement this function properly
+	// play the Chutes and Ladders game
+	void playGame();
+
+
 private:
-   const int WINNING_POSITION = 100;
-   
-   string winner;       // the winner
-   GameBoard gameBoard; // the game board
+	const int WINNING_POSITION = 100;
 
-   // TO DO: use ArrayQueue to store the players
+	string winner;       // the winner
+	GameBoard gameBoard; // the game board
+	int nPlayers;
+	// TO DO: use ArrayQueue to store the players
+	ArrayQueue<Player> store;
 };
