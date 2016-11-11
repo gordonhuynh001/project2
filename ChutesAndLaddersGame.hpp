61db8e8a874426b1e412b0f93c3101e0a28083a0
gordonhuynh001 <gordonhuynh001@csu.fullerton.edu>
2016-11-10 21:32:04 -0800

    Update ChutesAndLaddersGame.hpp

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -9,23 +9,18 @@
 
 using namespace std;
 
-// TO DO - change the default value to the number of your team members
 const int MIN_NUMBER_OF_PLAYERS = 2;
 
 class ChutesAndLaddersGame {
 public:
-	// TODO: implement the constructor with all your team members
 	// constructor with the default value of 2 minimum players
 	ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
 
-	// TODO: implement the destructor
 	// destructor
-	~ChutesAndLaddersGame();
+	~ChutesAndLaddersGame();	//all functions implemented in .cpp file
 
 	// accessors
-	// TO DO: implement the accessor to get the number Of Players
 	long getNumberOfPlayers() {
-		// TODO: implement this function properly
 		return nPlayers;
 	}
 
@@ -33,11 +28,9 @@ public:
 		return winner;
 	}
 
-	// TODO: implement this function properly
 	// reset the game - (1) this will reset the position of each player to zero
 	void resetGame();
 
-	// TODO: implement this function properly
 	// play the Chutes and Ladders game
 	void playGame();
 
@@ -48,6 +41,5 @@ private:
 	string winner;       // the winner
 	GameBoard gameBoard; // the game board
 	int nPlayers;
-	// TO DO: use ArrayQueue to store the players
 	ArrayQueue<Player> store;
 };
