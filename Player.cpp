c23de970e10960bea03cbd9883e54d5145a8abe5
gordonhuynh001 <gordonhuynh001@csu.fullerton.edu>
2016-11-10 21:42:07 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,18 +4,33 @@
 
 #include "Player.hpp"
 
-// TO DO: implement Player's assignment operator
+//Player's assignment operator
 Player& Player::operator=(const Player& p) {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
+	// TODO: implement this function properly
+	if (this != &p) {
+		die = p.die;
+		position = p.position;
+		playerName = p.playerName;
+	}
+	return *this;
 }
-
 // return player's new position after player rolls die
 // If the new position is outside of the board, player stays put
 // If not, player moves to the new square = player's postion + die's face value
-// TO DO: implement this function to move player to the new postion
-//        after player rolls die. Position must be inside the board
+//
 int Player::rollDieAndMove() {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
+	die.roll();
+	if ((position < 0) || (position >= BOARD_SIZE)) {		//checks if out of range on board
+		throw range_error("index out of bounds");
+	}
+
+	else if ((die.getFaceValue() + position) > 100) {		//checks if position will go over board and returns original position before roll
+
+		return position;
+	}
+
+	else {
+		position += die.getFaceValue();
+		return position;
+	};
 }
